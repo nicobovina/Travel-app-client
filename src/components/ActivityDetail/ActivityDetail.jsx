f5a2eff4f55f78632a31fad9ef0cc2e737c9224a
nicobovina <nicobovina@gmail.com>
2021-08-03 09:18:08 -0300

    .

--- src/components/ActivityDetail/ActivityDetail.jsx
+++ src/components/ActivityDetail/ActivityDetail.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
 // import actDetailStyle from './ActivityDetail.module.css';
-
-export function ActivityDetail({activity: {name, difficulty, duration, season}}){
-	console.log(name);
-	return (
-		<div className={actDetailStyle.activity}>
+/*
+<div className={actDetailStyle.activity}>
 			<h1>{name}</h1>
 			<div className={actDetailStyle.details}>
 				<div className={actDetailStyle.details_B}>
@@ -17,6 +14,29 @@ export function ActivityDetail({activity: {name, difficulty, duration, season}})
 					<p>{duration}</p>
 				</div>
 				<div className={actDetailStyle.details_B}>
+					<label htmlFor="season">Temporada</label>
+					<p>{season}</p>
+				</div>
+			</div>
+		</div>
+		*/
+
+
+
+export function ActivityDetail({activity: {name, difficulty, duration, season}}){
+	return (
+		<div >
+			<h1>{name}</h1>
+			<div >
+				<div >
+					<label htmlFor="difficulty">Dificultad</label>
+					<p>{difficulty}</p>
+				</div>
+				<div >
+					<label htmlFor="duration">Duracion</label>
+					<p>{duration}</p>
+				</div>
+				<div >
 					<label htmlFor="season">Temporada</label>
 					<p>{season}</p>
 				</div>
